dcd626a032c8f62787bf15dca3960de61ca4b286
Rishi Raj <rishiraj1096@gmail.com>
2025-04-27 20:15:43 +0530

    first commit

---
+++ middlewares/multiplemulter.js
@@ -0,0 +1,11 @@
+import multer from "multer";
+
+const storage = multer.memoryStorage();
+
+// Configure to handle multiple files
+export const multipleUpload = multer({ storage }).fields([
+  { name: "panImageFile", maxCount: 1 },
+  { name: "docFrontImageFile", maxCount: 1 },
+  { name: "docBackImageFile", maxCount: 1 },
+  { name: "passbookImageFile", maxCount: 1 }, // Add this line for passbook image
+]);
