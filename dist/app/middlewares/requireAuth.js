42b3a83bc9533349735cb48309b95f436ec6c6e7
Roshanchau <roshanchau001@gmail.com>
2025-06-20 09:31:25 +0545

    fix: fixes cookie based tracking.

--- dist/app/middlewares/requireAuth.js
+++ dist/app/middlewares/requireAuth.js
@@ -19,27 +19,26 @@ const config_1 = __importDefault(require("../config"));
 const prismaDb_1 = __importDefault(require("../db/prismaDb"));
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 exports.verifyToken = (0, catchAsyncError_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    const token = req.cookies.accessToken || "";
-    if (!token)
-        return res.status(401).json({ message: "unauthorized access" });
-    //   const authHeader = req.headers.authorization;
-    // if (!authHeader) {
-    //   return res.status(401).json({ error: 'No authorization header provided' });
-    // }
-    // // Extract the token (assuming Bearer scheme)
-    // const token = authHeader.split(' ')[1]; // Splits "Bearer tokenvalue" into ["Bearer", "tokenvalue"]
-    // console.log("token is", token);
+    // const token = req.cookies.accessToken || "";
+    // if (!token) return res.status(401).json({ message: "unauthorized access" });
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).json({ error: 'No authorization header provided' });
+    }
+    // Extract the token (assuming Bearer scheme)
+    const token = authHeader.split(' ')[1]; // Splits "Bearer tokenvalue" into ["Bearer", "tokenvalue"]
+    console.log("token is", token);
     if (!token) {
         return res.status(401).json({ error: 'Invalid authorization token' });
     }
     // Check if the token matches the one in cookies( to prvent CSRF attacks)
-    if (req.cookies.accessToken !== token) {
-        return (0, sendResponse_1.default)(res, {
-            statusCode: 401,
-            success: false,
-            message: "Unauthorized access",
-        });
-    }
+    // if(req.cookies.accessToken!==token){
+    //   return sendResponse(res, {
+    //       statusCode: 401,
+    //       success: false,
+    //       message: "Unauthorized access",
+    //   });
+    // }
     const decoded = jsonwebtoken_1.default.verify(token, config_1.default.jwt_access_secret);
     req.cookies.user = decoded; //set the cookies with the decoded token user(very imp).
     const user = yield prismaDb_1.default.user.findFirst({
